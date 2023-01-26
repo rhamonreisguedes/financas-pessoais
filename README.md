bc3ad93ddc91d7abf6c85a83edc0b0f937063a82
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-01-26 16:59:59 -0300

    initial set up and structure of front-end

---
+++ README.md
@@ -0,0 +1,9 @@
+Expense Tracker Project
+
+App created with React JS and Typescript.
+
+#########
+
+Projeto de Finanças Pessoais
+
+App criado com React JS e Typescript
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as C from './App.styles'
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+
+        {/*Área de Informações */}
+        
+        {/*Área de inserção*/}
+
+        {/* Tabela de itens */}
+        
+      </C.Body>
+    </C.Container>
+  );
+}
+
+export default App;
