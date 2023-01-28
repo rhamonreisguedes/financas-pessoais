485cee897d70d2882e6d3e958016db3f8e1d383d
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-01-28 12:38:42 -0300

    creating information area

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,23 @@ import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { items } from "./data/items";
 import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
-import {TableArea} from './components/TableArea'
+import { TableArea } from "./components/TableArea";
+import { InfoArea } from "./components/InfoArea";
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  };
 
   return (
     <C.Container>
@@ -22,12 +29,16 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        {/*Área de Informações */}
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
 
         {/*Área de inserção*/}
 
-        <TableArea list={filteredList}/>
-        
+        <TableArea list={filteredList} />
       </C.Body>
     </C.Container>
   );
--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -12,7 +12,7 @@ export const filterListByMonth = (list: Item[], date: string): Item[] => {
   for (let i in list) {
     if (
       list[i].date.getFullYear() === parseInt(year) &&
-      (list[i].date.getMonth() + 1) === parseInt(month)
+      list[i].date.getMonth() + 1 === parseInt(month)
     ) {
       newList.push(list[i]);
     }
@@ -21,13 +21,33 @@ export const filterListByMonth = (list: Item[], date: string): Item[] => {
   return newList;
 };
 
-
 export const formatDate = (date: Date): string => {
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
   let day = date.getDate();
 
-  return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`
-}
+  return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+};
 
-const addZeroToDate =  (n: number): string => n < 10 ? `0${n}` : `${n}`;
+const addZeroToDate = (n: number): string => (n < 10 ? `0${n}` : `${n}`);
+
+export const formatCurrentMonth = (currentMonth: string): string => {
+  let [year, month] = currentMonth.split("-");
+  let months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
+
+  return `${months[parseInt(month) - 1]} de ${year}`;
+};
